9e9dd10d13f2e37eb412e21a6aa72b6148738da3
Glauber Castro <63740083+GlauberMaster@users.noreply.github.com>
2023-11-29 01:27:24 +0000

    Ajustado a mensagem principal do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home(){
-    return <h1>Calma lá meu patrão</h1>
+    return <h1>Sheila minha gatona
+    Eu te amo muito!!!
+    Obrigado por me apoiar na compra do curso ❤❤❤❤
+    </h1>;
 }
 
 export default Home
